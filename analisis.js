8ec8f0da91e5a619906bf44ee189300f3d46d2cb
pablosc <pspablosanchezc@gmail.com>
2021-07-13 23:25:55 -0500

    creacion de ultimo proyecto

---
+++ analisis.js
@@ -0,0 +1,1 @@
+const salariosMex = Mexico.map();
---
+++ salarios.js
@@ -0,0 +1,82 @@
+const Mexico = [];
+
+Mexico.push({
+    name: "Paloma", 
+    salary: 2000,
+});
+Mexico.push({
+    name: "Tonatiuh",
+    salary: 50000,
+});
+Mexico.push({
+    name: "Ozay", 
+    salary: 24000,
+});
+Mexico.push({
+    name: "Azula", 
+    salary: 600,
+});
+Mexico.push({
+    name: "Pablo", 
+    salary: 2000,
+});
+Mexico.push({
+    name: "Daniel", 
+    salary: 3000,
+});
+Mexico.push({
+    name: "Rocío", 
+    salary: 1000,
+});
+Mexico.push({
+    name: "Edaí", 
+    salary: 30000,
+});
+Mexico.push({
+    name: "Alan", 
+    salary: 4500,
+});
+Mexico.push({
+    name: "Carlos", 
+    salary: 4500,
+});
+Mexico.push({
+    name: "Sandra", 
+    salary: 5000,
+});
+Mexico.push({
+    name: "Timmy", 
+    salary: 450000,
+});
+Mexico.push({
+    name: "Lilí", 
+    salary: 3000,
+});
+Mexico.push({
+    name: "Lety", 
+    salary: 15000,
+});
+Mexico.push({
+    name: "Juan", 
+    salary: 15000,
+});
+Mexico.push({
+    name: "Johann", 
+    salary: 15000,
+});
+Mexico.push({
+    name: "Ana", 
+    salary: 8000,
+});
+Mexico.push({
+    name: "Tony", 
+    salary: 1000,
+});
+Mexico.push({
+    name: "Jonathan", 
+    salary: 30000,
+});
+Mexico.push({
+    name: "Manchas", 
+    salary: 24000,
+});
